50f9cf629b1c0965c409773d56cbe09b38b14a0d
Swapanjeet <swapanjeet@gmail.com>
2020-08-06 23:36:58 +0530

    RxSwift
    Login using RxSwift

--- RxSwfitLogin/ViewController.swift
+++ RxSwfitLogin/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
+    
+    private let loginViewModel = LoginViewModel()
+    private let disposeBag = DisposeBag()
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBAction func tappedLoginButton(_ sender: UIButton) {
+        print("Tapped Login")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        usernameTextField.becomeFirstResponder()
+        
+        
+        usernameTextField.rx.text.map {$0 ?? "" }.bind(to: loginViewModel.usernameTextPublishSubject).disposed(by: disposeBag)
+        passwordTextField.rx.text.map {$0 ?? "" }.bind(to: loginViewModel.passwordTextPublishSubject).disposed(by: disposeBag)
+        
+        loginViewModel.isValid().bind(to: loginButton.rx.isEnabled).disposed(by: disposeBag)
+        loginViewModel.isValid().map{$0 ? 1 : 0.1 }.bind(to: loginButton.rx.alpha).disposed(by: disposeBag)
+        
     }
 
 
 }
 
+class LoginViewModel {
+    let usernameTextPublishSubject = PublishSubject<String>()
+    let passwordTextPublishSubject = PublishSubject<String>()
+    
+    func isValid() -> Observable<Bool> {
+        return Observable.combineLatest(usernameTextPublishSubject.asObservable().startWith(""), passwordTextPublishSubject.asObservable().startWith("")).map { username, password in
+            return username.count > 3 && password.count > 3
+        }.startWith(false)
+    }
+}
